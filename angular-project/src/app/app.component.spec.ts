250ec02f2d1cfee4ededae306535845f2f86391d
Sachin Mishra <Sachin.Mishra@gep.com>
2019-04-30 16:06:05 +0530

    Added the changes related to compoents

--- angular-project/src/app/app.component.spec.ts
+++ angular-project/src/app/app.component.spec.ts
@@ -26,10 +26,17 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-project');
   });
 
-  it('2. should render title in a h1 tag', () => {
+  it('3. should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular Devops');
   });
+
+  it('3. Should give me the addition of the Numbers', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    expect(fixture.debugElement.componentInstance.add(10,20)).toBe(30);
+  });
+
+
 });
