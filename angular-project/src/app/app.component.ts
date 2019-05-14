0441ae27698dc50c66613c6188d30adc75ce0520
Sachin Mishra <sachinmishra609@gmail.com>
2019-05-15 00:01:27 +0530

    Update app.component.ts

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-project';
+  title = 'angular-Dem-project';
 
   public add(a: any, b: any) {
     return Number.parseInt(a) + Number.parseInt(b);
